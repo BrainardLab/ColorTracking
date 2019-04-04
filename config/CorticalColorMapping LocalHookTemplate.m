99b0a52df9ad210e8146386af4510f945ef51e56
Michael Barnett <micalan@sas.upenn.edu>
2019-04-04 17:46:45 -0400

    made TbTb-able and trying to play with DKL stuff

---
+++ config/CorticalColorMapping LocalHookTemplate.m
@@ -0,0 +1,55 @@
+function CorticalColorMappingLocalHook
+%  LFContrastAnalsysisLocalHook
+%
+% Configure things for working on the  LFContrastAnalysis project.
+%
+% For use with the ToolboxToolbox.
+%
+% If you 'git clone' ILFContrastAnalsysis into your ToolboxToolbox "projectRoot"
+% folder, then run in MATLAB
+%   tbUseProject('LFContrastAnalysis')
+% ToolboxToolbox will set up IBIOColorDetect and its dependencies on
+% your machine.
+%
+% As part of the setup process, ToolboxToolbox will copy this file to your
+% ToolboxToolbox localToolboxHooks directory (minus the "Template" suffix).
+% The defalt location for this would be
+%   ~/localToolboxHooks/LFContrastAnalsysisLocalHook.m
+%
+% Each time you run tbUseProject('LFContrastAnalysis'), ToolboxToolbox will
+% execute your local copy of this file to do setup for LFContrastAnalsysis.
+%
+% You should edit your local copy with values that are correct for your
+% local machine, for example the output directory location.
+%
+
+
+%% Say hello.
+fprintf('CorticalColorMapping local hook.\n');
+projectName = 'CorticalColorMapping';
+
+%% Delete any old prefs
+if (ispref(projectName))
+    rmpref(projectName);
+end
+
+%% Specify base paths for materials and data
+[~, userID] = system('whoami');
+userID = strtrim(userID);
+% switch userID
+%     case {'dhb'}
+%     otherwise
+% end
+%
+%% Specify where output goes
+% 
+% if ismac
+%     % Code to run on Mac plaform
+%     setpref(projectName,'');
+% elseif isunix
+% elseif ispc
+%     % Code to run on Windows platform
+%     warning('No supported for PC')
+% else
+%     disp('What are you using?')
+% end
---
+++ tutorials/meanShift.m
@@ -0,0 +1,88 @@
+% mean shiting an image demo
+load('sampleLMS.mat')
+figure
+subplot(4, 1, 1);
+imshow(LMSimage);
+title('LMS Image')
+% Compute stats.
+
+bg = [mean(mean(LMSimage(:,:,1))), mean(mean(LMSimage(:,:,2))), mean(mean(LMSimage(:,:,3)))];
+
+deltaLMSimage = cat(3,LMSimage(:,:,1)-bg(1),LMSimage(:,:,2)-bg(2),LMSimage(:,:,3)-bg(3));
+
+[calFormatScaledLMSimage,nCols,mRows] = ImageToCalFormat(LMSimage);
+
+subplot(4, 1, 2);
+imshow(deltaLMSimage, []);title('Delta LMS')
+
+% 3. set up M raw 
+
+Mraw = [ 1, 0, 0;...
+         1, -bg(1)/bg(2), 0;...
+        -1, -1, (bg(1)+bg(2))/bg(3)];
+
+% 4. invert Mraw    
+MrawInv = inv(Mraw);
+
+% 5. get the 3 isolating mechanisms 
+isochrom_raw = MrawInv(:,1);
+rgisolum_raw = MrawInv(:,2);
+sisolum_raw = MrawInv(:,3);
+
+% 6. get pooled contrast per mechanism 
+isochrom_raw_pooled = norm(isochrom_raw./bg);
+rgisolum_raw_pooled = norm(rgisolum_raw./bg);
+sisolum_raw_pooled  = norm(sisolum_raw./bg);
+
+% 7. scale each mechanism by the pooled contrast to achieve unit length
+isochrom_unit = isochrom_raw/isochrom_raw_pooled;
+rgisolum_unit = rgisolum_raw/rgisolum_raw_pooled;
+sisolum_unit  = sisolum_raw/sisolum_raw_pooled;
+
+% 8. Compute the values of the normalizing contants by plugging in the unit
+% iso 
+lum_resp_raw = Mraw *isochrom_unit;
+lminM_resp_raw = Mraw *rgisolum_unit;
+SminLum_resp_raw = Mraw *sisolum_unit;
+
+% Scaling the rows of Mraw to get unit response
+D_scale = [1/lum_resp_raw(1), 0, 0;...
+           0, 1/lminM_resp_raw(2), 0;...
+           0, 0, 1/SminLum_resp_raw(3)];
+M = D_scale*Mraw;
+
+% get new M_inv 
+M_inv = inv(M);
+
+% DKL coords
+DKL_coords = M*calFormatScaledLMSimage;
+
+DKLimage  = CalFormatToImage(DKL_coords, nCols,mRows);
+subplot(4, 1, 3);
+imshow(DKLimage,[]); title('DKL image')
+
+
+scaledDKLcoords = DKL_coords./max(abs(DKL_coords(:)));
+scaledDKLimage  = CalFormatToImage(scaledDKLcoords, nCols,mRows);
+subplot(4, 1, 4);
+imshow(scaledDKLimage,[]); title('DKL image')
+
+
+figure
+scatter3(DKL_coords(1,:),DKL_coords(2,:),DKL_coords(3,:))
+
+figure
+scatter3(scaledDKLcoords(1,:),scaledDKLcoords(2,:),scaledDKLcoords(3,:))
+
+
+
+% 
+% % Shift to desired mean
+% R2 = img2(:,:,1) .* (bg(1)/meanRGB(1));
+% G2 = img2(:,:,2) .* (bg(2)/meanRGB(2));
+% B2 = img2(:,:,3) .* (bg(3)/meanRGB(3));
+% 
+% img2(:,:,1) = LMSimage(:,:,1)./max(LMSimage(:,:,1));
+% img2(:,:,2) = LMSimage(:,:,2)./max(LMSimage(:,:,2));
+% img2(:,:,3) = LMSimage(:,:,3)./max(LMSimage(:,:,3));
+% imshow(img2);title('unit max per channel')

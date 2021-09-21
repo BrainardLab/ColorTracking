7c1bd20630928bdb952a8cfc0395daca88b5c683
Michael B <micalan@sas.upenn.edu>
2021-09-21 17:24:40 -0400

    removed scaling

--- tutorials/makeChromaticGaborImage.m
+++ tutorials/makeChromaticGaborImage.m
@@ -14,15 +14,6 @@ nMonitorBits = 8;
 nInputLevels = 2.^nMonitorBits;
 CalibrateFitGamma(calStructOBJ, nInputLevels);
 
-%% Get relevant info from cal file 
-% Get wavelength sampling of functions in cal file.
-S = calStructOBJ.get('S');
-wavelengthAxis = SToWls(S);
-
-ambient = calStructOBJ.get('P_ambient');
-P_device      = calStructOBJ.get('P_device');
-gammaInput = calStructOBJ.get('gammaInput');
-gammaTable = calStructOBJ.get('gammaTable');
 
 %% Load Stockman and Sharpe 2 deg Cone Fund.
 load T_cones_ss2
@@ -35,7 +26,7 @@ fxCPD      = 1;
 angleDeg   = 0;
 phaseDeg   = 0;
 sigmaDeg   = 0.6;
-[scaledContrastImage, ~] = generateStimContrastProfile(imgSzXYdeg,smpPerDeg,fxCPD,angleDeg,phaseDeg,sigmaDeg);
+contrastImage = generateStimContrastProfile(imgSzXYdeg,smpPerDeg,fxCPD,angleDeg,phaseDeg,sigmaDeg);
         
 %% Standard initialization of calibration structure
 SetSensorColorSpace(calStructOBJ,T_cones_ss2,S_cones_ss2);
@@ -52,7 +43,7 @@ backgroundPrimaries_hat = SensorToPrimary(calStructOBJ,backgroundLMS_hat);
 %% Create a chromatic modulation 
 %LMScontrastModulation = .10*[.7071 -.7071 0]';
 LMScontrastModulation = .09*[1 0 0]';
-[stimSettings,stimExcitations,imgInfo] = generateChromaticGabor(calStructOBJ,scaledContrastImage,backgroundLMS_hat,LMScontrastModulation);
+[stimSettings,stimExcitations,imgInfo] = generateChromaticGabor(calStructOBJ,contrastImage,backgroundLMS_hat,LMScontrastModulation);
 
 %% Display the Gabor
 hFig = figure(1); clf;
@@ -60,4 +51,4 @@ set(hFig, 'Position', [800 10  400 500]);
 imshow(stimSettings); axis 'image'; axis 'ij'
 set(gca, 'XColor', 'none', 'YColor', 'none', 'XTick', [], 'YTick', [], 'FontSize', 14);
 box off
-set(hFig, 'Color', backgroundPrimaries);
+set(hFig, 'Color', PrimaryToSettings(calStructOBJ, backgroundPrimaries));

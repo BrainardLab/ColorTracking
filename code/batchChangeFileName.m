f262c19b76fc160ec51c58c63b0f95c2847c1210
Michael B <micalan@sas.upenn.edu>
2021-10-28 20:47:03 -0400

    script to change from LSM to LS1

---
+++ code/batchChangeFileName.m
@@ -0,0 +1,26 @@
+% Batch rename folder name for the hard drive to handle mismatch with the
+% dropbox account
+projectName = 'CorticalColorMapping';
+basePath = getpref(projectName,'dropboxPath');
+
+subjs = {'BMC','KAS','MAB'};
+
+
+%% loop over subjects
+for curr_subj = 1:length(subjs)
+    
+    subjectDir = fullfile(basePath,'CNSC_data','ColorTrackingTask','LS1',subjs{curr_subj});
+    
+    dataFileName = dir(subjectDir);
+    dataFileIndx = find(contains({dataFileName(:).name},'LMS'));
+    
+    for ii = 1:length(dataFileIndx)
+        oldFileName = fullfile(subjectDir,dataFileName(dataFileIndx(ii)).name);
+        newFileName = strrep(fullfile(subjectDir,dataFileName(dataFileIndx(ii)).name),'LMS','LS1');
+        
+        
+        str = ['mv ' oldFileName ' ' newFileName];
+        
+        unix(str)
+    end
+end

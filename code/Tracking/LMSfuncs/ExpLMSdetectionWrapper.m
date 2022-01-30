8bd8fa489182204b86c48d6378d5b35e418b6183
Ben Chin <ben.chinny@gmail.com>
2022-01-30 11:30:35 -0500

    ExpLMSdetectionWrapper: tailor contrasts for different subjects

--- code/Tracking/LMSfuncs/ExpLMSdetectionWrapper.m
+++ code/Tracking/LMSfuncs/ExpLMSdetectionWrapper.m
@@ -6,16 +6,28 @@
 % preserve the practice of running positive and negative angles of cone
 % contrast modulations in separate blocks. 
 
+subjName = 'KAS';
+
 % SET RANDOM SEED
 randSeed = 2;
 rng(randSeed);
 
-targetContrast1 = 1.5.*[0.0016 0.0032 0.0048 0.0064 0.008 0.0096 0.002 0.004 0.006 0.008 0.01 0.012 0.002 0.004 0.006 0.008 0.01 0.012 0.0065 0.013 0.0195 0.026 0.0325 0.039 0.0065 0.013 0.0195 0.026 0.0325 0.039 0.013 0.026 0.039 0.052 0.065 0.078]';
-targetContrastAngle1 = [0 0 0 0 0 0 45 45 45 45 45 45 -45 -45 -45 -45 -45 -45 75 75 75 75 75 75 -75 -75 -75 -75 -75 -75 90 90 90 90 90 90]';
-targetContrast2 = -1.5.*[0.0016 0.0032 0.0048 0.0064 0.008 0.0096 0.002 0.004 0.006 0.008 0.01 0.012 0.002 0.004 0.006 0.008 0.01 0.012 0.0065 0.013 0.0195 0.026 0.0325 0.039 0.0065 0.013 0.0195 0.026 0.0325 0.039 0.013 0.026 0.039 0.052 0.065 0.078]';
-targetContrastAngle2 = [0 0 0 0 0 0 45 45 45 45 45 45 -45 -45 -45 -45 -45 -45 75 75 75 75 75 75 -75 -75 -75 -75 -75 -75 90 90 90 90 90 90]';
-targetContrast = [targetContrast1; targetContrast2];
+% targetContrast1 = 1.5.*[0.0016 0.0032 0.0048 0.0064 0.008 0.0096 0.002 0.004 0.006 0.008 0.01 0.012 0.002 0.004 0.006 0.008 0.01 0.012 0.0065 0.013 0.0195 0.026 0.0325 0.039 0.0065 0.013 0.0195 0.026 0.0325 0.039 0.013 0.026 0.039 0.052 0.065 0.078]';
+targetContrastAngle1 = [-75 -75 -75 -75 -75 -75 -45 -45 -45 -45 -45 -45 0 0 0 0 0 0 45 45 45 45 45 45 75 75 75 75 75 75 90 90 90 90 90 90]';
+% targetContrast2 = -1.5.*[0.0016 0.0032 0.0048 0.0064 0.008 0.0096 0.002 0.004 0.006 0.008 0.01 0.012 0.002 0.004 0.006 0.008 0.01 0.012 0.0065 0.013 0.0195 0.026 0.0325 0.039 0.0065 0.013 0.0195 0.026 0.0325 0.039 0.013 0.026 0.039 0.052 0.065 0.078]';
+targetContrastAngle2 = targetContrastAngle1;
+% targetContrast = [targetContrast1; targetContrast2];
 targetContrastAngle = [targetContrastAngle1; targetContrastAngle2];
+
+if strcmp(subjName,'BMC')
+    targetContrast1 = [linspace(0.04/6,0.04,6) linspace(0.0125/6,0.0125,6) linspace(0.01/6,0.01,6) linspace(0.0125/6,0.0125,6) linspace(0.04/6,0.04,6) linspace(0.09/6,0.09,6)]';
+elseif strcmp(subjName,'MAB')
+    targetContrast1 = [linspace(0.07/6,0.07,6) linspace(0.02/6,0.02,6) linspace(0.025/6,0.025,6) linspace(0.025/6,0.025,6) linspace(0.095/6,0.095,6) linspace(0.18/6,0.18,6)]';
+elseif strcmp(subjName,'KAS')
+    targetContrast1 = [linspace(0.06/6,0.06,6) linspace(0.0175/6,0.0175,6) linspace(0.01/6,0.01,6) linspace(0.0175/6,0.0175,6) linspace(0.06/6,0.06,6) linspace(0.14/6,0.14,6)]';
+end
+targetContrast2 = -targetContrast1;
+targetContrast = [targetContrast1; targetContrast2];
 
 % SPECIFY NUMBER OF REPEATS 
 nRepeats = 20;

ad1202884a41df1acc621d96edacc8e30b30fa14
David Brainard <brainard@psych.upenn.edu>
2022-03-24 15:03:46 -0400

    Fixed

--- tutorials/paramsConversionCheck.m
+++ tutorials/paramsConversionCheck.m
@@ -1,6 +1,6 @@
 %% parameter conversion check
 
-% fitDemoTwoMechanisms.m
+% Initialize
 close all;
 clear all;
 
@@ -20,52 +20,92 @@ timebase = 1:length(lagVec);
 thePacket.stimulus.values   = [cL(:),cS(:)]';
 thePacket.stimulus.timebase = timebase;
 
-% The Kernel 
+% The kernel 
 thePacket.kernel.values = [];
 thePacket.kernel.timebase = [];
 
-%% Make the fit one mechanism object
+%% Make the RotM version object
 theDimension= size(thePacket.stimulus.values, 1);
 ctmRotMOBJ = tfeCTMRotM('verbosity','none','dimension',theDimension, 'numMechanism', 2 ,'fminconAlgorithm','active-set');
 
-%% Make the fit two mechanism object
+%% Make the "classic" two mechanism version object
 theDimension= size(thePacket.stimulus.values, 1);
 ctmOBJmechTwo = tfeCTM('verbosity','none','dimension',theDimension, 'numMechanism', 2 ,'fminconAlgorithm','active-set');
 
 %% Set Up RotM params to generate lags
-params.angle = 80;
-params.minAxisRatio = .1;
-params.scale = 2;
-params.amplitude = .4;
-params.minLag    = .35;
+rotMParams.angle = 80;
+rotMParams.minAxisRatio = .1;
+rotMParams.scale = 2;
+rotMParams.amplitude = .4;
+rotMParams.minLag    = .35;
 
-% Convert the RotM params to the 2 mech orth. params (if this works, this
+%% Convert the RotM params to the 2 mech orth. params (if this works, this
 % should produce tha same lags from both models given the smae input)
-R = deg2rotm(params.angle);
-E = [1,0;0,params.minAxisRatio];
-theWeights = R*E*params.scale;
+classicParams = ParamsRotMToClassic(rotMParams);
 
-params2.weightL_1 =theWeights(1,1);
-params2.weightS_1 =theWeights(1,2);
-weightL_2 =theWeights(2,1);
-weightS_2 =theWeights(2,2);
-if abs(weightL_2./ params2.weightS_1) == abs(params2.weightL_1./ weightS_2)
-    params2.weight_M2 = abs(weightL_2./ params2.weightS_1);
-else 
-    error('Check the weights!!!')
-end
-params2.minLag    =params.minLag;
-params2.amplitude =params.amplitude ;
+% See if we can go back again
+rotMParamsCheck = ParamsClassicToRotM(classicParams)
 
-
-%% compute the lag for our stim and the params above
+%% Compute the lag for our stim and the params above
 % the RotM mode
-lagsFromRotM = ctmRotMOBJ.computeResponse(params,thePacket.stimulus,thePacket.kernel);
+lagsFromRotM = ctmRotMOBJ.computeResponse(rotMParams,thePacket.stimulus,thePacket.kernel);
 
 % 2 mech with orth. constraint
-lagsFromTwoMech = ctmOBJmechTwo.computeResponse(params2,thePacket.stimulus,thePacket.kernel);
+lagsFromTwoMech = ctmOBJmechTwo.computeResponse(classicParams,thePacket.stimulus,thePacket.kernel);
 
 %% plot it
 figure; hold on 
 plot(lagsFromRotM.values,'LineWidth',2,'Color','k');
 plot(lagsFromTwoMech.values,'LineWidth',1.5,'Color','r','LineStyle','--');
+
+
+function classicParams = ParamsRotMToClassic(rotMParams)
+
+R = deg2rotm(rotMParams.angle);
+E = [1,0;0,rotMParams.minAxisRatio];
+theWeights = (R*E*rotMParams.scale)';
+
+classicParams.weightL_1 = theWeights(1,1);
+classicParams.weightS_1 = theWeights(1,2);
+weightL_2 = theWeights(2,1);
+weightS_2 = theWeights(2,2);
+
+if (abs( weightL_2 / classicParams.weightS_1 - weightS_2 / classicParams.weightL_1) > 1e-10)
+    classicParams.weight_M2 = abs(weightL_2 ./ classicParams.weightS_1);
+else 
+    error('Check the weights!!!')
+end
+
+classicParams.minLag    = rotMParams.minLag;
+classicParams.amplitude = rotMParams.amplitude;
+
+end
+
+
+function rotMParams = ParamsClassicToRotM(classicParams)
+
+% Get angle
+rotMParams.angle = atand(classicParams.weightS_1/classicParams.weightL_1);
+
+% Rotate mechanism 1 back
+invR = deg2rotm(-rotMParams.angle);
+
+% Get raw mechanism 1 to get scale factor
+rawMech1 = invR*[classicParams.weightL_1 classicParams.weightS_1]';
+
+% Pull out scale factor
+rotMParams.scale = rawMech1(1);
+
+% Get scaled S cone weight
+weightS_1 = classicParams.weightS_1/rotMParams.scale;
+
+% Find orthogonal direction
+weightL_2 = classicParams.weight_M2*weightS_1;
+weightS_2 = classicParams.weight_M2;
+rotMParms.minAxisRatio = norm([weightL_2 weightS_2]);
+
+
+rotMParams.minLag = classicParams.minLag;
+rotMParams.amplitude = classicParams.amplitude;
+
+end

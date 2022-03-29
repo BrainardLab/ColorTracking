e47b8a8f941a136a09f34bccad5ce0478d120784
David Brainard <brainard@psych.upenn.edu>
2022-03-29 18:50:54 -0400

    Update.

--- tutorials/testFitTwoMechRotM.m
+++ tutorials/testFitTwoMechRotM.m
@@ -40,30 +40,34 @@ theDimension= size(thePacket.stimulus.values, 1);
 ctmRotMOBJ= tfeCTMRotM('verbosity','none','dimension',theDimension, 'numMechanism', 2 ,'fminconAlgorithm','active-set');
 
 %% set params to see if they can be recovered
-% params.angle        = 75;
-% params.minAxisRatio = .2;
-% params.scale        = .2;
-% params.amplitude    = .4;
-% params.minLag       = .35;
-params.weightL_1 = 1;
-params.weightS_1 = 1;
-params.weight_M2 = -3;
-params.minLag    = 0.350;
-params.amplitude = 0.450;
+paramsSimulateRotM.angle        = 80;
+paramsSimulateRotM.minAxisRatio = .2;
+paramsSimulateRotM.scale        = .2;
+paramsSimulateRotM.amplitude    = .7;
+paramsSimulateRotM.minLag       = .25;
+% params.weightL_1 = 2;
+% params.weightS_1 = 1;
+% params.weight_M2 = 0.72;
+% params.minLag    = 0.350;
+% params.amplitude = 0.450;
+
+paramsSimulateClassic = ParamsRotMToClassic(paramsSimulateRotM);
+
 
 % compute the lag for our stim and the params above
-lagsFrom2Mech = ctmOBJ.computeResponse(params,thePacket.stimulus,thePacket.kernel);
+lagsFromRotM = ctmRotMOBJ.computeResponse(paramsSimulateRotM,thePacket.stimulus,thePacket.kernel);
+lagsFromClassic = ctmOBJ.computeResponse(paramsSimulateClassic,thePacket.stimulus,thePacket.kernel);
 
 %% finish the packet
 % the lags from the model
-thePacket.response.values   = lagsFrom2Mech.values;
-thePacket.response.timebase = lagsFrom2Mech.timebase;
+thePacket.response.values   = lagsFromRotM.values;
+thePacket.response.timebase = lagsFromRotM.timebase;
 % The Meta Data
 thePacket.metaData.stimDirections = atand(cS(:)./cL(:));
 thePacket.metaData.stimContrasts  = vecnorm([cS(:),cL(:)]')';
 
 %% fit the packet (try to recover the params)
-fitErrorScalar = 10000;
+fitErrorScalar = 100000;
 defaultParamsInfo = [];
 [classicParams,fVal,classicResponses] = ctmOBJ.fitResponse(thePacket,'defaultParamsInfo',defaultParamsInfo,...
     'initialParams',[], 'fitErrorScalar',fitErrorScalar);
@@ -72,10 +76,13 @@ fitErrorScalar = 100000;
 [rotMParams,fVal,rotmResponses] = ctmRotMOBJ.fitResponse(thePacket,'defaultParamsInfo',defaultParamsInfo,...
     'initialParams',[], 'fitErrorScalar',fitErrorScalar);
 
-classicParamsCheck = ParamsRotMToClassic(rotMParams)
-
+% classicParamsCheck = ParamsRotMToClassic(rotMParams)
+rotMParamsCheck = ParamsClassicToRotM(classicParams)
 
 figure; hold on
-plot(lagsFrom2Mech.values,'LineWidth',3,'Color','k')
+plot(lagsFromRotM.values,'LineWidth',3,'Color','k')
+plot(lagsFromClassic.values,'LineWidth',3,'Color','b')
+
+
 plot(rotmResponses.values,'LineWidth',2,'Color','r')
 plot(classicResponses.values,'LineWidth',1,'Color','g')

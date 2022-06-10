5ef7087cf287160d1c66255383a56d6c44ad4349
Michael B <micalan@sas.upenn.edu>
2022-06-10 17:51:30 -0400

    started crossval model comp code

---
+++ code/Analysis/modelComparison/crossValLagDet.m
@@ -0,0 +1,27 @@
+function [] = crossValLagDet(subjID,varargin)
+
+% Cross-Validation model comaprison code.
+p = inputParser; p.KeepUnmatched = true; p.PartialMatching = false;
+p.addRequired('subjID',@ischar);
+p.addParameter('fitMethod','LGS',@ischar);
+p.addParameter('nCrossValIter',15,@isnumeric);
+p.addParameter('makeAndSavePlot',false,@islogical);
+p.addParameter('fitErrorScalar',1000,@isnumeric);
+p.parse(subjID,varargin{:});
+
+% get subject code
+if strcmp(subjID,'MAB')
+    subjCode = 'Subject1';
+elseif strcmp(subjID,'BMC')
+    subjCode = 'Subject2';
+elseif strcmp(subjID,'KAS')
+    subjCode = 'Subject3';
+end
+
+% get prefs
+projectName = 'ColorTracking';
+crossValCacheFolder = getpref(projectName,'crossValCacheFolder');
+
+% Load the cross val data cache
+dataCacheName = fullfile(crossValCacheFolder,[subjCode '_crossVal_chached.mat']);
+load(dataCacheName);

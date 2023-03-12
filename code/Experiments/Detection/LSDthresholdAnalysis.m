eff14e505991d20853ccab6ed7e239c40a19bc0d
Ben Chin <ben.chinny@gmail.com>
2023-03-11 22:38:55 -0800

    update threshold plot

--- code/Experiments/Detection/LSDthresholdAnalysis.m
+++ code/Experiments/Detection/LSDthresholdAnalysis.m
@@ -28,6 +28,12 @@ targetContrastAngleUnq = unique(S.targetContrastAngle);
 
 nRepeats = 10;
 
+if exist('p.Results.tFitBoot','var')
+    bIncomingBoots = true;
+else
+    bIncomingBoots = false; 
+end
+
 for i = 1:length(targetContrastAngleUnq)
    ind = abs(S.targetContrastAngle-targetContrastAngleUnq(i))<0.01;
    for j = 1:nRepeats
@@ -50,6 +56,9 @@ for i = 1:length(targetContrastAngleUnq)
    bFit(i) = bFitTmp(indBest);
    PCdta(:,i) = PCdtaTmp(:,indBest);
 end
+
+tFitBoot = [];
+mFitBoot = [];
 
 if p.Results.nBoot>0
     for i = 1:p.Results.nBoot
@@ -98,7 +107,7 @@ end
 if p.Results.showPlot
     figure;
     set(gcf,'Position',[315 73 1240 863]);
-    for i = 1:length(targetContrastAngleUnq)
+    for i = 1:6
         ind = abs(S.targetContrastAngle-targetContrastAngleUnq(i))<0.01;
         contrastMinMax = [min(abs(S.targetContrast(ind))) max(abs(S.targetContrast(ind)))];
         contrastUnq = unique(abs(S.targetContrast(ind)));
@@ -121,6 +130,49 @@ if p.Results.showPlot
         axis square; 
         formatFigure('Michelson Contrast (%)','Proportion Correct',[num2str(targetContrastAngleUnq(i)) 'deg, T=' num2str(tFit(i)*100,2) '%']);
     end
+    if length(targetContrastAngleUnq)>6
+        figure;
+        set(gcf,'Position',[315 73 1240 863]);
+        for i = 7:length(targetContrastAngleUnq)
+            ind = abs(S.targetContrastAngle-targetContrastAngleUnq(i))<0.01;
+            contrastMinMax = [min(abs(S.targetContrast(ind))) max(abs(S.targetContrast(ind)))];
+            contrastUnq = unique(abs(S.targetContrast(ind)));
+            contrasts4plot = (contrastMinMax(1)-diff(contrastMinMax)*0.1):0.0001:(contrastMinMax(2)+diff(contrastMinMax)*0.1);
+            if strcmp(p.Results.fitType,'weibull')
+                PCfit = psyfitWeibullfunc(zeros(size(contrasts4plot)),contrasts4plot,mFit(i),sFit(i),bFit(i),DPcrt,nIntrvl,0);
+            elseif strcmp(p.Results.fitType,'gaussian')
+                PCfit = psyfitgengaussfunc(zeros(size(contrasts4plot)),contrasts4plot,mFit(i),sFit(i),bFit(i),DPcrt,nIntrvl,0);
+            end
+            subplot(2,3,i-6);
+            hold on;
+            plot(contrasts4plot.*100,PCfit,'k-');
+            plot(contrastUnq.*100,PCdta(:,i),'ko','MarkerSize',10,'MarkerFaceColor','w');
+            xlimTmp = xlim;
+            plot([tFit(i)*100 tFit(i)*100],[0.4 normcdf(0.5.*sqrt(nIntrvl).*DPcrt)],'k--');
+            plot([xlimTmp(1) tFit(i)*100],[normcdf(0.5.*sqrt(nIntrvl).*DPcrt) normcdf(0.5.*sqrt(nIntrvl).*DPcrt)],'k--');
+            text(xlimTmp(2)*0.65,0.5,['nTrials=' num2str(sum(ind))]);
+            ylim([0.4 1]);
+            xlim(xlimTmp);
+            axis square; 
+            formatFigure('Michelson Contrast (%)','Proportion Correct',[num2str(targetContrastAngleUnq(i)) 'deg, T=' num2str(tFit(i)*100,2) '%']);
+        end        
+    end
+    
+    if ~isempty(p.Results.tFitBoot)
+        tFitCI = quantile(p.Results.tFitBoot,[0.16 0.84],2);
+        for i = 1:length(targetContrastAngleUnq)
+            if targetContrastAngleUnq(i) ==0
+               tFitPlotLSci1(i,:) = tFitCI(i,1).*100.*[1 0];
+               tFitPlotLSci2(i,:) = tFitCI(i,2).*100.*[1 0];
+            elseif targetContrastAngleUnq(i)==90
+               tFitPlotLSci1(i,:) = tFitCI(i,1).*100.*[0 1]; 
+               tFitPlotLSci2(i,:) = tFitCI(i,2).*100.*[0 1]; 
+            else
+               tFitPlotLSci1(i,:) = tFitCI(i,1).*100.*[1 tand(targetContrastAngleUnq(i))]./norm([1 tand(targetContrastAngleUnq(i))]);
+               tFitPlotLSci2(i,:) = tFitCI(i,2).*100.*[1 tand(targetContrastAngleUnq(i))]./norm([1 tand(targetContrastAngleUnq(i))]);
+            end            
+        end
+    end
     
     for i = 1:length(targetContrastAngleUnq)
         if targetContrastAngleUnq(i) ==0
@@ -131,16 +183,21 @@ if p.Results.showPlot
            tFitPlotLS(i,:) = tFit(i).*100.*[1 tand(targetContrastAngleUnq(i))]./norm([1 tand(targetContrastAngleUnq(i))]);
         end
     end
-
+    
+    scaleThreshPlot = max(abs([tFitPlotLSci1(:,2); tFitPlotLSci2(:,2)]));
+    
     figure;
-    plot(tFitPlotLS(:,1),tFitPlotLS(:,2),'kx','MarkerSize',10);
     hold on;
-    plot([-5 5],[0 0],'k-');
-    plot([0 0],[-5 5],'k-');
+    plot(scaleThreshPlot.*[-1.1 1.1],[0 0],'k-');
+    plot([0 0],scaleThreshPlot.*[-1.1 1.1],'k-');    
+    plot(tFitPlotLS(:,1),tFitPlotLS(:,2),'k.','MarkerSize',10,'MarkerFaceColor','w');
+    for i = 1:length(targetContrastAngleUnq)
+        plot([tFitPlotLSci1(i,1) tFitPlotLSci2(i,1)],[tFitPlotLSci1(i,2) tFitPlotLSci2(i,2)],'k-','LineWidth',1.5);
+    end
     axis square;
     formatFigure('L','S');
-    xlim([-5 5]);
-    ylim([-5 5]);
+    xlim(scaleThreshPlot.*[-1.1 1.1]);
+    ylim(scaleThreshPlot.*[-1.1 1.1]);
 end
 
 end

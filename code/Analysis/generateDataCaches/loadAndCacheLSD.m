7b428e278b101dc9210da05ddc7304a4ee93fdfb
David Brainard <brainard@psych.upenn.edu>
2024-01-19 09:39:47 -0500

    Get code that summarized the detection experiment working for me.

--- code/Analysis/generateDataCaches/loadAndCacheLSD.m
+++ code/Analysis/generateDataCaches/loadAndCacheLSD.m
@@ -1,9 +1,25 @@
 function [] = loadAndCacheLSD(subjID)
+%
+% This function is the top level call to read in and do
+% some massaging and initial fitting of th detection
+% experiment.
+%
+% You need to have your Matlab preference file set up
+% to point to teh folder containing the raw data - see
+% config/ColorTrackingLocalHookTemplate.m for how you
+% set that up.  Then execute the three lines below to
+% produce the cached data files used for further fitting.
+% The cached data is put into a filename
+% with pcCache as part of it, where pc is supposed to make
+% you think of percent correct.
+
 %{
     loadAndCacheLSD('MAB');
+    loadAndCacheLSD('BMC');
+    loadAndCacheLSD('KAS');
 %}
 
-% get subject code
+% Get subject code
 if strcmp(subjID,'MAB')
     subjCode = 'Subject1';
 elseif strcmp(subjID,'BMC')
@@ -12,26 +28,28 @@ elseif strcmp(subjID,'KAS')
     subjCode = 'Subject3';
 end
 
+% Burge lab functions used to read and sort the data, etc.
 S = loadPSYdataLMSall('JND', subjID, 'LSD', 'CGB',{[1:48]}, 'blobfish', 'local');
 
+% Get angles and contrast and convert to L and S cone contrast
 theAngles = unique(S.targetContrastAngle)';
-
 [targetContrast,targetAngles] = getContrastLSD(subjID,'combined');
 targetContrast = flipud(targetContrast);
 anglesMat = repmat(theAngles,[size(targetContrast,1),1]);
-
-% Convert angles to L and S contrast
 cS = targetContrast.*sind(anglesMat);
 cL = targetContrast.*cosd(anglesMat);
 
+% Burge lab functions used to do more stuff.  It looks like
+% our main purpose here is to get the percent correct data
+% out for each stimulus level.
 [tFit,mFit,sFit,bFit,PCdta] = LSDthresholdAnalysis(S,1,'bPLOTpsfs',1,'fitType','weibull','showPlot',false);
-
 pcData = flipud(PCdta);
-
 infoParams.computeDate = date;
-%% save out the params
+
+%% Save out what we need for more fitting
 projectName = 'ColorTracking';
 paramsCacheFolder = getpref(projectName,'paramsCacheFolder');
 paramsCacheFileName     = fullfile(paramsCacheFolder,'detection',[subjCode '_pcCache.mat']);
 save(paramsCacheFileName,'infoParams','pcData','targetContrast','cL','cS','theAngles')
+
 end

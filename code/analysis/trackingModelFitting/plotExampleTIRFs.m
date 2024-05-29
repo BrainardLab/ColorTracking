903cd9947283fa0f2a39d11ea92760bce6580892
Ben Chin <ben.chinny@gmail.com>
2024-05-29 06:49:18 -0700

    histograms of errors

--- code/analysis/trackingModelFitting/plotExampleTIRFs.m
+++ code/analysis/trackingModelFitting/plotExampleTIRFs.m
@@ -4,7 +4,7 @@
 % from raw data.  You have to save by hand.
 
 % Set up the subj and exp
-subjID = 'BMC';
+subjID = 'KAS';
 expName = 'LS1';
 numRuns = 20;
 fitMethod = 'LGS';
@@ -22,3 +22,23 @@ S = structElementSelect(Sall,ind,size(Sall.tgtXmm,2));
 
 % Fit the log gaussian and plot
 [r, rSmooth, rParams]= LMSxcorrAnalysis(S,fitMethod,'bPLOTfitsAndRaw',true);
+
+%%
+
+absMaxContrastLMSunq = unique(abs(S.MaxContrastLMS),'rows');
+indCheck = [];
+
+figure;
+set(gcf,'Position',[447 247 1182 646]);
+for i = 1:length(absMaxContrastLMSunq)
+   subplot(2,3,i);
+   ind = abs(abs(S.MaxContrastLMS(:,3))-absMaxContrastLMSunq(i,3))<0.001;
+   indCheck = [indCheck; find(ind)];
+   tgtXmmLtmp = S.tgtXmmL(:,ind);
+   rspXmmLtmp = S.rspXmmL(:,ind);
+   signedErrRsp = rspXmmLtmp-tgtXmmLtmp;
+   signedErrDeg = atand(signedErrRsp./925);
+   hist(signedErrDeg(:),21);
+   axis square;
+   formatFigure('Error (deg)','Count',['S contrast = ' num2str(absMaxContrastLMSunq(i,3),2)]);
+end
